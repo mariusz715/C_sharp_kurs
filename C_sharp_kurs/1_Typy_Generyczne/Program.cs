283e7727e2177e1193224ef5ae79ec5f7b10168d
MARIUSZ <MARIUSZ@ACER>
2019-03-07 22:58:02 +0100

    KolekcjaKolowa

--- C_sharp_kurs/1_Typy_Generyczne/Program.cs
+++ C_sharp_kurs/1_Typy_Generyczne/Program.cs
@@ -10,6 +10,28 @@ namespace _1_Typy_Generyczne
     {
         static void Main(string[] args)
         {
+            var kolejka = new KolejkaKolowa(pojemnosc: 4);
+
+
+
+
+            var zmienna = 0.0;
+
+            while (double.TryParse(Console.ReadLine(), out zmienna))
+            {
+                kolejka.zapis(zmienna);
+            }
+
+
+            Console.WriteLine("W kolejce znajduja sie liczby :");
+            while (!kolejka.pusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Wypisz());
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
+
