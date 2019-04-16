37e291fc0d0e72b4799e50991365cabd7958d407
MARIUSZ <MARIUSZ@ACER>
2019-04-17 00:42:55 +0200

    Kolekcja_2

---
+++ C_sharp_kurs/2_Kolekcje_Generyczne/Pracownik.cs
@@ -0,0 +1,8 @@
+﻿namespace _2_Kolekcje_Generyczne
+{
+    internal class Pracownik
+    {
+        public string imie{ get; set; }
+        public string nazwisko { get; set; }
+    }
+}
--- C_sharp_kurs/2_Kolekcje_Generyczne/Program.cs
+++ C_sharp_kurs/2_Kolekcje_Generyczne/Program.cs
@@ -9,7 +9,22 @@ namespace _2_Kolekcje_Generyczne
     class Program
     {
         static void Main(string[] args)
+
         {
+            Pracownik[] pracownicy = new Pracownik[]
+            {
+                new Pracownik{imie ="krol",nazwisko="AVB"  },
+                new Pracownik{imie ="pop",nazwisko="AVB"  },
+                new Pracownik{imie ="koks",nazwisko="AVB"  },
+
+        };
+            foreach(var praca in pracownicy)
+            {
+
+                Console.WriteLine(praca.imie + " " + praca.nazwisko);
+            }
+
+
         }
     }
 }
